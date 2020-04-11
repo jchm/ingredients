40fe6125caab6d0a285e28a690bbcba451ea97bd
Jochem Schutte <jochemschutte@gmail.com>
2020-04-11 23:08:43 +0200

    change favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
         content: process.env.npm_package_description || ''
       }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🍽</text></svg>' }]
   },
   /*
    ** Customize the progress-bar color
