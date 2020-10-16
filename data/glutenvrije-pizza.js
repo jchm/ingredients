c6901cda26268c58c920fc248e1b5ccd8d7faa12
Jochem <jochemschutte@gmail.com>
2020-10-16 16:41:19 +0200

    Create glutenvrije-pizza.js

---
+++ data/glutenvrije-pizza.js
@@ -0,0 +1,34 @@
+{
+  "meta": {
+    "name": "Glutenvrije pizza",
+    "description": "Voor 6 pizza's",
+    "persons": 6
+  },
+  "ingriedents": [
+    {
+      "amount": 1000,
+      "ingriedent": "Bloem",
+      "measurement": "gr"
+    },
+    {
+      "amount": 800,
+      "ingriedent": "Water",
+      "measurement": "gr"
+    },
+    {
+      "amount": 15,
+      "ingriedent": "Gist",
+      "measurement": "gr"
+    },
+    {
+      "amount": 35,
+      "ingriedent": "Olijfolie",
+      "measurement": "gr"
+    },
+    {
+      "amount": 35,
+      "ingriedent": "Zout",
+      "measurement": "gr"
+    }
+  ]
+}
